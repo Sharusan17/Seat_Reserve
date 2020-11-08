fda52693ff2782180b6de83bb778547ff547075a
Sharusan17 <67514731+Sharusan17@users.noreply.github.com>
2020-11-08 15:36:19 +0000

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,12 @@ import java.text.NumberFormat;
 import java.util.Random;
 import java.util.Scanner;
 
-//todo: While loop
-//todo: firstclass seats
-//todo: If booking is no and reserving is no, it should exit the program
 
 public class Main {
 
     public static void main(String[] args) {
+
+        int num_people = 0;
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("               Welcome To EasyJet!                  ");
@@ -21,7 +20,7 @@ public class Main {
         String seat_class = " ";
         if (book_ticket == true) {
             System.out.println("How many people do you want book?");
-            int num_people = scanner.nextInt();
+            num_people = scanner.nextInt();
             String s = scanner.nextLine();
             System.out.println("First Class or Economy Class?");
             seat_class = scanner.nextLine();
@@ -34,14 +33,13 @@ public class Main {
                 System.out.println("You have selected " + num_people + " Economy Class Ticket!");
                 System.out.println("------------------------------------------------------");
             }
-        }else{
         }
 
-        String firstclass = NumberFormat.getCurrencyInstance().format(200);
-        String economyclass = NumberFormat.getCurrencyInstance().format(150);
+        String firstclass = NumberFormat.getCurrencyInstance().format(900 * num_people);
+        String economyclass = NumberFormat.getCurrencyInstance().format(400 * num_people);
 
-        String firstclass_extraleg = NumberFormat.getCurrencyInstance().format(215);
-        String economyclass_extraleg = NumberFormat.getCurrencyInstance().format(165);
+        String firstclass_extraleg = NumberFormat.getCurrencyInstance().format(915 * num_people);
+        String economyclass_extraleg = NumberFormat.getCurrencyInstance().format(415 * num_people);
 
         int rows = 40;
         int rowsleg = 15;
@@ -95,98 +93,108 @@ public class Main {
             boolean legroom = extralegs.equals("Y") || extralegs.equals("y");
 
             if (reserve_seat.equals("Y") || reserve_seat.equals("y")) {
-                if (legroom == true) {
-                    System.out.println("Enter the row:(1-15)");
-                    int selectrow = scanner.nextInt();
-                    System.out.println("Enter the column:(1-6)");
-                    int selectcol = scanner.nextInt();
-                    while (true) {
-                        if (selectrow < 1 || selectrow > 15 || selectcol < 1 || selectcol > 6) {
-                            System.out.println("Choose from 1-15");
-                            selectrow = scanner.nextInt();
-                            System.out.println("Choose from 1-6");
-                            selectcol = scanner.nextInt();
-                            continue;
+                for (int b = 1; b<=num_people; b++) {
+                    if (legroom == true) {
+                        System.out.println("Enter the row for ticket "+b+":(1-15)");
+                        int selectrow = scanner.nextInt();
+                        System.out.println("Enter the column for ticket "+b+":(1-6)");
+                        int selectcol = scanner.nextInt();
+                        while (true) {
+                            if (selectrow < 1 || selectrow > 15 || selectcol < 1 || selectcol > 6) {
+                                System.out.println("Choose from 1-15");
+                                selectrow = scanner.nextInt();
+                                System.out.println("Choose from 1-6");
+                                selectcol = scanner.nextInt();
+                                continue;
+                            }
+                            System.out.print("Your seat for ticket " + b + ": [" + selectrow + "," + selectcol + "]");
+                            System.out.println(" ");
+                            break;
                         }
-                        System.out.println("Your Selected Seat is [" + selectrow + "," + selectcol + "]");
-                        break;
-                    }
-                } else {
-                    System.out.println("Enter the row:(15-40)");
-                    int selectrow = scanner.nextInt();
-                    System.out.println("Enter the column:(1-6)");
-                    int selectcol = scanner.nextInt();
-                    while (true) {
-                        if (selectrow < 15 || selectrow > 40 || selectcol < 1 || selectcol > 6) {
-                            System.out.println("Choose from 15-40");
-                            selectrow = scanner.nextInt();
-                            System.out.println("Choose from 1-6");
-                            selectcol = scanner.nextInt();
-                            continue;
+                    } else {
+                        for (b = 1; b<=num_people; b++) {
+                            System.out.println("Enter the row for ticket " + b + ":(15-40)");
+                            int selectrow = scanner.nextInt();
+                            System.out.println("Enter the column for ticket " + b + ":(1-6)");
+                            int selectcol = scanner.nextInt();
+                            while (true) {
+                                if (selectrow < 15 || selectrow > 40 || selectcol < 1 || selectcol > 6) {
+                                    System.out.println("Choose from 15-40");
+                                    selectrow = scanner.nextInt();
+                                    System.out.println("Choose from 1-6");
+                                    selectcol = scanner.nextInt();
+                                    continue;
+                                }
+                                System.out.print("Your seat for ticket " + b + ": [" + selectrow + "," + selectcol + "]");
+                                System.out.println(" ");
+                                break;
+                            }
                         }
-                        System.out.println("Your Selected Seat is [" + selectrow + "," + selectcol + "]");
-                        break;
                     }
                 }
 
-            } else if (reserve_seat.equals("N") || (reserve_seat.equals("n"))) {
-                if (legroom == true) {
-                    Random random = new Random();
-                    int randomrowslegs = random.nextInt(rowsleg);
-                    int randomcols = random.nextInt(cols);
-                    System.out.print("Your seat is [" + randomrowslegs + "," + randomcols + "]");
-                    System.out.println(" ");
-                } else {
-                    Random random = new Random();
-                    int randomrows = random.nextInt(rows);
-                    int randomcols = random.nextInt(cols);
-                    System.out.print("Your seat is [" + randomrows + "," + randomcols + "]");
-                    System.out.println(" ");
+
+            }else if (reserve_seat.equals("N") || (reserve_seat.equals("n"))) {
+                for (int b = 1; b<=num_people; b++) {
+                    if (legroom == true) {
+                        Random random = new Random();
+                        int randomrowslegs = random.nextInt(rowsleg);
+                        int randomcols = random.nextInt(cols);
+                        System.out.print("Your seat for ticket " + b + ": [" + randomrowslegs + "," + randomcols + "]");
+                        System.out.println(" ");
+                    } else {
+                        Random random = new Random();
+                        int randomrows = random.nextInt(rows);
+                        int randomcols = random.nextInt(cols);
+                        System.out.print("Your seat for ticket " + b + ": [" + randomrows + "," + randomcols + "]");
+                        System.out.println(" ");
+                    }
                 }
             }
 
+
             if (legroom == true) {
                 System.out.println("---------------------------------------------");
-                System.out.println("Price of ticket: " + firstclass_extraleg + "(inc.£15 for legroom)");
+                System.out.println("Price of ticket for "+num_people+" people : "+ firstclass_extraleg + "(inc.£15 for legroom)");
                 System.out.println("---------------------------------------------");
             } else {
                 System.out.println("------------------------");
-                System.out.println("Price of ticket: " + firstclass);
+                System.out.println("Price of ticket for " +num_people+" people : " + firstclass);
                 System.out.println("------------------------");
             }
         }
 
         else if (seat_class.equals("Economy Class") || seat_class.equals("economy class")) {
-
             System.out.println("•Extra Leg Room?(Y/N)");
             String extralegs = scanner.nextLine();
             boolean legroom = extralegs.equals("Y") || extralegs.equals("y");
 
-            if (legroom == true) {
-                Random random = new Random();
-                int randomrowslegs = random.nextInt(rowsleg);
-                int randomcols = random.nextInt(cols);
-                System.out.print("Your seat is [" + randomrowslegs + "," + randomcols + "]");
-                System.out.println(" ");
-            } else {
-                Random random = new Random();
-                int randomrows = random.nextInt(rows);
-                int randomcols = random.nextInt(cols);
-                System.out.print("Your seat is [" + randomrows + "," + randomcols + "]");
-                System.out.println(" ");
+            for (int b = 1; b<=num_people; b++) {
+                if (legroom == true) {
+                    Random random = new Random();
+                    int randomrowslegs = random.nextInt(rowsleg);
+                    int randomcols = random.nextInt(cols);
+                    System.out.print("Your seat for ticket " + b + ": [" + randomrowslegs + "," + randomcols + "]");
+                    System.out.println(" ");
+                } else {
+                    Random random = new Random();
+                    int randomrows = random.nextInt(rows);
+                    int randomcols = random.nextInt(cols);
+                    System.out.print("Your seat for ticket " + b + ": [" + randomrows + "," + randomcols + "]");
+                    System.out.println(" ");
+                }
             }
 
             if (legroom == true) {
                 System.out.println("---------------------------------------------");
-                System.out.println("Price of ticket: " + economyclass_extraleg + "(inc.£15 for legroom)");
+                System.out.println("Price of ticket for "+num_people+" people : "+ economyclass_extraleg + "(inc.£15 for legroom)");
                 System.out.println("---------------------------------------------");
             } else {
                 System.out.println("------------------------");
-                System.out.println("Price of ticket: " + economyclass);
+                System.out.println("Price of ticket for " +num_people+" people: " + economyclass);
                 System.out.println("------------------------");
             }
         }
         System.out.println("Thanks For Visiting");
     }
 }
-
